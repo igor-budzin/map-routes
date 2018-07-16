bba4bc422154b60d8a38f01309c54980c0b85ae2
Igor <igor.budzin95@gmail.com>
2018-07-16 15:46:26 +0300

    saved routes

--- src/SavedRoutes.js
+++ src/SavedRoutes.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {Button} from 'antd';
+import 'antd/lib/button/style';
+
 
 export default class SavedRoutes extends React.Component {
 	constructor(props) {
@@ -6,15 +9,42 @@ export default class SavedRoutes extends React.Component {
 	}
 	render() {
 		const routes = JSON.parse(localStorage.getItem('localRoutes'));
-		let abc = routes.forEach((item, index) => index);
-		console.log(routes)
+		console.log(routes);
 		return (
 			<div className="page-container">
 				<h2>Saved routes</h2>
-				<div>
-				</div>
+
+				<table className="saved-routes-list">
+					<thead>
+						<tr>
+							<td>Route Name</td>
+							<td>Route Points</td>
+							<td>Distance</td>
+							<td></td>
+						</tr>
+					</thead>
+					<tbody>
+						{
+							routes.map((item, index) => {
+								let point = item.routeItems.map((route) => route.main_text);
+								return (
+									<tr className="route" key={item.routeName + index}>
+										<td>{item.routeName}</td>
+										<td>{point.join(', ')}</td>
+										<td>{`${item.distance} km`}</td>
+										<td>
+											<span className="btn-wrap">
+												<Button type="primary">Edit</Button>
+												<Button type="danger">Delete</Button>
+											</span>
+										</td>
+									</tr>
+								)
+							})
+						}
+					</tbody>
+				</table>
 			</div>
-			
 		);
 	}
 }
